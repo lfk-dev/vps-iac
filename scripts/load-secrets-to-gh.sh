ed651f982660dbea874b08e5efbecd5fa6a115c4
Lukasz Feliks Kowalski <lukaszkowalski5040@gmail.com>
2026-01-28 12:12:17 +0100

    refactor(gha): improve deployment workflow

--- scripts/load-secrets-to-gh.sh
+++ scripts/load-secrets-to-gh.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 
-# extracts the directory of the script using combination of dirname, cd, bash builtins and pwd
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# assumes single file in dotenv style, with key=value pairs, no parentesis or comments
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 <file>, while in git repo"
+    exit 1
+fi
 
-# TODO: i don't know if i like the secrets.txt location handling :)
-cat $SCRIPT_DIR/../secrets.txt | while read line
-do
-    if [[ ! $line == \#* ]] && [ -n "$line" ]; then
-        # checks if the line is not a comment line or empty line
-        
-        # extracts two fileds with `=` as delimiter
-        name=$(echo $line | cut -d= -f 1)
-        value=$(echo $line | cut -d= -f 2-) # the `-` helps if there are more `=` in the value
-        echo "Name: $name, value: $value" 
-        echo $value | gh secret set $name # gh assumes remote of the current repo as the target
-    fi
-done
+while IFS= read -r line; do
+  [[ -z "$line" || "$line" =~ ^# ]] && continue
+  key="${line%%=*}" #key is before the first =
+  value="${line#*=}" # value after the first =
+  echo "$value" | gh secret set "$key"
+done < $1
